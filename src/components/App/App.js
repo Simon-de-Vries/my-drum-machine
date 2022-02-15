65b5d005ebb20fca27a68b17a91717820ec8b8cb
Simon de Vries <simon.vries@stud.h-da.de>
2022-02-15 20:14:39 +0100

    Update Touch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,10 +10,18 @@ function App() {
     <div className={styles.app}>
       <h1>drum machine</h1>
       <div className={styles.drumkitButtonContainer}>
-        <div className={styles.drumkitButton} onClick={() => setToggle(false)}>
+        <div
+          className={styles.drumkitButton}
+          onClick={() => setToggle(false)}
+          onTouchStart={() => setToggle(false)}
+        >
           <h2>metal</h2>
         </div>
-        <div className={styles.drumkitButton} onClick={() => setToggle(true)}>
+        <div
+          className={styles.drumkitButton}
+          onClick={() => setToggle(true)}
+          onTouchStart={() => setToggle(true)}
+        >
           <h2>indie</h2>
         </div>
       </div>
