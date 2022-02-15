4bed07fd839e4307300e2c843a82b2b6e66ea8fa
Simon de Vries <simon.vries@stud.h-da.de>
2022-02-15 21:30:40 +0100

    Button-Feedback

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,31 @@ import assets from "../../assets.js";
 import { useState } from "react";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className={styles.app}>
       <h1>drum machine</h1>
       <div className={styles.drumkitButtonContainer}>
-        <div className={styles.drumkitButton} onClick={() => setToggle(false)}>
+        <button
+          className={
+            toggle === false
+              ? styles.drumkitButtonPressed
+              : styles.drumkitButton
+          }
+          onClick={() => setToggle(false)}
+        >
           <h2>metal</h2>
-        </div>
-        <div className={styles.drumkitButton} onClick={() => setToggle(true)}>
+        </button>
+
+        <button
+          className={
+            toggle === true ? styles.drumkitButtonPressed : styles.drumkitButton
+          }
+          onClick={() => setToggle(true)}
+        >
           <h2>indie</h2>
-        </div>
+        </button>
       </div>
       <div className={styles.container}>
         {assets.map((element) => (
